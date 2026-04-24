ec079250c091457d0628273fc0706ee5e23dfbf9
javicvjavicv-ctrl <javicvjavicv@gmail.com>
2026-04-24 08:23:02 +0200

    Add files via upload

---
+++ functions/api/justwatch.js
@@ -0,0 +1,42 @@
+export async function onRequestPost(context) {
+  try {
+    const body = await context.request.text();
+    
+    const resp = await fetch('https://apis.justwatch.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+        'Origin': 'https://www.justwatch.com',
+        'Referer': 'https://www.justwatch.com/',
+        'App-Version': '3.0.0'
+      },
+      body: body
+    });
+
+    const data = await resp.text();
+
+    return new Response(data, {
+      status: resp.status,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  } catch (err) {
+    return new Response(JSON.stringify({ errors: [{ message: err.message }] }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+    });
+  }
+}
+
+export async function onRequestOptions() {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    }
+  });
+}
